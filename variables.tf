a4610a4ebb81953a6d6be448d8c9c25f31309733
Oleksandr Korniienko <okornii@softserveinc.com>
2019-04-17 23:31:42 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,34 +3,8 @@ variable "elastic-clusters" {
   default = [""]
 }
 
-variable "cluster_id" {}
-
-variable "cluster_description" {}
-
-variable "node_type" {
-  default = "cache.m3.medium"
-}
-
-variable "num_cache_nodes" {
-  default = 3
-}
-
-variable "parameter_group_name" {
-  default = "default.redis4.0"
-}
-
-variable "engine_version" {
-  default = 4.0
-}
-
-variable "subnet_group_name" {}
 variable "vpc_id" {}
 
 variable "availability_zones" {
   type = "list"
-}
-
-
-variable "snapshot_retention_limit" {
-  default = 0
 }
