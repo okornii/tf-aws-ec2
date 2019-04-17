10d9a7be694dde4d3e15666908ff0a3e1c0c2157
Oleksandr Korniienko <okornii@softserveinc.com>
2019-04-17 23:44:57 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_elasticache_replication_group" "redis_cluster" {
   count           = "${length(var.elastic-clusters)}"
 
-  replication_group_id = "${lookup(var.elastic-clusters[count.index], cluster_id)}"
-  replication_group_description = "${lookup(var.elastic-clusters[count.index], cluster_description)}"
-  node_type = "${lookup(var.elastic-clusters[count.index], node_type)}"
+  replication_group_id = "${lookup(var.elastic-clusters[count.index], "cluster_id")}"
+  replication_group_description = "${lookup(var.elastic-clusters[count.index], "cluster_description")}"
+  node_type = "${lookup(var.elastic-clusters[count.index], "node_type")}"
   port = 6379
-  parameter_group_name = "${lookup(var.elastic-clusters[count.index], parameter_group_name)}"
+  parameter_group_name = "${lookup(var.elastic-clusters[count.index], "parameter_group_name")}"
   automatic_failover_enabled = true
   availability_zones = ["${var.availability_zones}"]
-  security_group_ids = ["${lookup(var.elastic-clusters[count.index], securiy_group_id)}"]
-  number_cache_clusters = "${lookup(var.elastic-clusters[count.index], num_cache_nodes)}"
-  snapshot_retention_limit = "${lookup(var.elastic-clusters[count.index], snapshot_retention_limit)}"
-  engine_version = "${lookup(var.elastic-clusters[count.index], engine_version)}"
+  security_group_ids = ["${lookup(var.elastic-clusters[count.index], "securiy_group_id")}"]
+  number_cache_clusters = "${lookup(var.elastic-clusters[count.index], "num_cache_nodes")}"
+  snapshot_retention_limit = "${lookup(var.elastic-clusters[count.index], "snapshot_retention_limit")}"
+  engine_version = "${lookup(var.elastic-clusters[count.index], "engine_version")}"
 }
